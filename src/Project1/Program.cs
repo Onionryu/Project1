aab251c26cb81ea0e958cdc1bc77f730b84802eb
Onionryu <Onionryu@DESKTOP-IB6NMAG>
2017-09-25 01:05:20 +0300

    Added Hello world

--- src/Project1/Program.cs
+++ src/Project1/Program.cs
@@ -9,6 +9,8 @@ namespace Project1
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("Hello world");
+            Console.ReadLine();
         }
     }
 }
